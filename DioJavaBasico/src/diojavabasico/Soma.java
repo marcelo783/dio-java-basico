efff043d9534d389bb797ce2fcf3e9e5d180b54a
marcelo783 <marcelo.oliveira7931@gmail.com>
2023-09-01 22:08:32 -0300

    meu primeiro commit

---
+++ DioJavaBasico/src/diojavabasico/Soma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diojavabasico;
+
+/**
+ *
+ * @author MARCELO
+ */
+public class Soma {
+     public static void main(String[] args) {
+          int n1 = 10;
+    int n2 = 20;
+    
+    int s = n1 + n2;
+    
+        System.out.println(s);
+        
+    }
+    
+  
+    
+}
